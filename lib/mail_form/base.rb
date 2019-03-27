4abdcbb76ab67f2ca9be57d979e54ccc71d550f3
Nate Kandler <natekandler@gmail.com>
2019-03-26 19:16:00 -0700

    add clear attribute method

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,7 +1,14 @@
 module MailForm
   class Base
+    include ActiveModel::AttributeMethods
+    attribute_method_prefix 'clear_'
     def self.attributes(*names)
       attr_accessor(*names)
+      define_attribute_methods(names)
+    end
+
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
     end
   end
 end
