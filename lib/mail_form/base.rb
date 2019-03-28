e1ec26e6a0e49ce802e6c0873310440548453757
Nate Kandler <natekandler@gmail.com>
2019-03-27 19:26:13 -0700

    add Active Model compliance

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,14 +1,31 @@
 module MailForm
   class Base
     include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
+
     attribute_method_prefix 'clear_'
+    attribute_method_suffix '?'
+
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
     end
 
+    protected
+
     def clear_attribute(attribute)
       send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
+    end
+
+    def persisted?
+      false
     end
   end
 end
